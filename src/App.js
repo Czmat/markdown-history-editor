cfa90acbe55068301a9d5dbe94e98294f3f060c4
Chaise Matev <aj_fl81@msn.com>
2020-01-29 15:11:16 -0500

    have marked text working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import marked from 'marked';
 import './App.css';
 
 function App() {
+  const [markup, setMarkup] = useState({ text: '' });
+
+  const onChange = e => {
+    markup.text = e.target.value;
+
+    setMarkup({ text: e.target.value });
+    console.log(markup.text);
+  };
+
+  marked.setOptions({
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    langPrefix: 'language-',
+    highlight: function(code, lang) {
+      if (lang === 'js') {
+        return code;
+      }
+      return code;
+    },
+  });
+
+  function createMarkup() {
+    var convText = marked(markup.text, {
+      sanitize: true,
+    });
+
+    return { __html: convText };
+  }
+
+  function MyComponent() {
+    return <div dangerouslySetInnerHTML={createMarkup()} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea className="text-field" onChange={onChange}>
+            {markup.text}
+          </textarea>
+        </div>
+        <div className="col-sm-6">{MyComponent()}</div>
+      </div>
+      <button type="button" class="btn btn-outline-secondary">
+        Secondary
+      </button>
     </div>
   );
 }
